e247a10492a6bcb57f192c0c0e5287a12c9b7d08
William Walsh <wwalsh@control4.com>
2020-06-25 13:30:27 -0400

    do full build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "${0%/*}"
 python3 ./buildERB.py
-#sudo gem install bundler
-#sudo bundle install
-#bundle exec middleman build --clean --verbose
+sudo gem install bundler
+sudo bundle install
+bundle exec middleman build --clean --verbose
