676dd0d2bda4da344692a3cdf6dd95429ce2fb35
William Walsh <wwalsh@control4.com>
2020-06-25 13:54:58 -0400

    change bundle install dir

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 cd "${0%/*}"
 python3 ./buildERB.py
 sudo gem install bundler
-sudo bundle config path vendor/bundle
+sudo bundle config path ./vendor/bundle
 sudo bundle install --jobs 4 --retry 3
 bundle exec middleman build --clean --verbose
