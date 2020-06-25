5ed68da47e4b75e9e9c0d47042e44ee6659cb654
William Walsh <wwalsh@control4.com>
2020-06-25 13:16:30 -0400

    just build ERB for now

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp -r ./docs/ ./
 python3 ./buildERB.py
-sudo gem install bundler
-sudo bundle install
-bundle exec middleman build --clean --verbose
+#sudo gem install bundler
+#sudo bundle install
+#bundle exec middleman build --clean --verbose
