87b517f1848abb24a52f8b39f00af5cc9b32d1b7
William Walsh <54117394+WWalshC4@users.noreply.github.com>
2020-12-23 20:22:03 +0000

    Install exact version of bundler
    see: https://bundler.io/blog/2019/05/14/solutions-for-cant-find-gem-bundler-with-executable-bundle.html

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 cd "${0%/*}"
 python3 ./buildERB.py
-sudo gem install bundler
+sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
 bundle config path ./vendor/bundle
 bundle install --jobs 4 --retry 3
 bundle exec middleman build --clean --verbose
