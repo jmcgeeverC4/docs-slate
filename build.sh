8c705acd65ef1a8a2ced41cdd3f2e98adbd111c9
William Walsh <wwalsh@control4.com>
2024-05-23 12:26:33 +0100

    add -c true arg to build.sh for building 2column view

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 # Copyright 2020 Wirepath Home Systems, LLC. All rights reserved.
 
 cd "${0%/*}"
+while getopts c: name
+do
+	case $name in
+	c)	if [ $OPTARG = "true" ]; then
+			mv ./source/stylesheets/screen.css.scss ./source/stylesheets/screen_old.css.scss
+			mv ./source/stylesheets/nolang.css.scss ./source/stylesheets/screen.css.scss
+		fi
+	esac
+done
 python3 ./buildERB.py
 sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
 bundle config path ./vendor/bundle
